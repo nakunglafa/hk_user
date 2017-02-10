f3297a757ec6932ccf65e5aaaf91bd0556e02762
krishna <nakunglafa@gmail.com>
2017-02-10 09:32:26 +0545

    Revert "Revert "Revert "'start'"""
    This reverts commit d2ecb20cfbdb98912c347836ae1ea392c75a3096.

--- js/config.js
+++
@@ -1,9 +0,0 @@
-app.config(function($stateProvider){
-
-	$stateProvider
-		.state('index',{
-			url:'/',
-			templateUrl:'template/home.html',
-			controller:'homectrl as home'
-		});
-})
--- js/controller/indexctrl.js
+++
@@ -1,3 +0,0 @@
-app.controller('indexctrl',function($scope){
-	$scope.title=title;
-})
